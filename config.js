7ecc2375e9991b4c56a069139a3b38bf1b42d893
Christopher Cates <ccates1@ggc.edu>
2017-02-08 17:31:05 -0500

    More node setup/initialization tweeks.

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'url' : 'mongodb://ccates1:florida2@ds147599.mlab.com:47599/dailydish'
+};
--- server.js
+++ server.js
@@ -1,23 +1,43 @@
 var aync = require('async');
-var bcrypt = require('bcryptjs')
+var bcrypt = require('bcryptjs');
 var bodyParser = require('body-parser');
+var config = require('./config.js');
 var cors = require('cors');
 var express = require('express');
 var jwt = require('jwt-simple');
+var morgan = require('morgan');
 var moment = require('moment');
 var mongoose = require('mongoose');
+var path = require('path');
 var request = require('request');
-
+/*
+ |--------------------------------------------------------------------------
+ | express/middleware
+ |--------------------------------------------------------------------------
+ */
 var app = express();
 app.set('port', process.env.PORT || 3198);
 app.use(cors({
-    origin: '*',
-    withCredentials: false,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin' ]
+  origin: '*',
+  withCredentials: false,
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin' ]
 }));
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 app.use(express.static(path.join(__dirname, 'www')));
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+/*
+ |--------------------------------------------------------------------------
+ | mongoose/MongoDB
+ |--------------------------------------------------------------------------
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(config.url);
+mongoose.connection.on('error', function(err) {
+  console.log('Error: Could not connect to MongoDB.');
+});
