a02bf494cfb2636278d61905bc19554730a2f99f
Christopher Cates <ccates1@ggc.edu>
2017-02-10 17:38:24 -0500

    Added login form.

--- web/www/scripts/controllers.js
+++ web/www/scripts/controllers.js
@@ -1,5 +1,23 @@
 angular.module('dailydish.controllers', [])
 .controller('LoginCtrl', function($scope) {
+  $scope.register = false;
+  $(function() {
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
 
 })
 .controller('DashboardCtrl', function($scope) {
